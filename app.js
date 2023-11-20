59a07af2ab74473242c321529010c0772f1ca2ae
Bassem <hemaiziabassem@gmail.com>
2023-11-20 16:47:22 +0100

    cors

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const db_connect = require('./config/db');
-const dotenv = require('dotenv');
-const swaggerSetup = require('./swagger');
+const express = require("express");
+const db_connect = require("./config/db");
+const dotenv = require("dotenv");
+const swaggerSetup = require("./swagger");
 dotenv.config();
 const cors = require("cors");
-const corsOptions = require('./config/corsOptions');
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors(corsOptions));
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 db_connect();
 
-app.use('/user', require('./routes/user'));
-app.use('/movies', require('./routes/movie'));
-app.use('/series', require('./routes/serie'));
+app.use("/user", require("./routes/user"));
+app.use("/movies", require("./routes/movie"));
+app.use("/series", require("./routes/serie"));
 
 swaggerSetup(app);
 
-app.listen(PORT, ()=>{
-    console.log('server start working');
-})
+app.listen(PORT, () => {
+  console.log("server start working");
+});
